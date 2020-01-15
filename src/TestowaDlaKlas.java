d7d1134b25789c6d98f7f944bfee58c7b20862c9
FoxOasis <foxiaty@gmail.com>
2020-01-15 22:56:10 +0100

    Palindrome

---
+++ src/TestowaDlaKlas.java
@@ -0,0 +1,12 @@
+public class TestowaDlaKlas {
+
+    static PalindromChecker pali = new PalindromChecker();
+
+
+
+    public static void main(String[] args) {
+
+    System.out.println(pali.isPalindrom("ala"));
+    }
+
+}
