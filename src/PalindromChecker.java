26f290da65fe99ea32e0a4cee8c09c1914ced1ae
FoxOasis <foxiaty@gmail.com>
2020-01-15 22:55:32 +0100

    Palindrome

---
+++ src/PalindromChecker.java
@@ -0,0 +1,20 @@
+public class PalindromChecker {
+
+
+        public boolean isPalindrom(String ciag) {
+
+            char[] zbior = ciag.toCharArray();
+
+            for (int pozycja = 0; pozycja < zbior.length; pozycja++) {
+                if (zbior[pozycja] != (zbior[zbior.length - 1 - pozycja])) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+
+
+
+
